e6d0b064db23aab8dbee0115e9d4b1b7b80391d5
mittalvaibhav1 <mittalvaibhavandroid@gmail.com>
2021-01-15 23:43:06 +0530

    face recoginition done

--- testing/src/Videocam.js
+++ testing/src/Videocam.js
@@ -1,6 +1,5 @@
 import { useEffect, useRef} from 'react';
 import * as faceapi from 'face-api.js';
-import { MtcnnOptions } from 'face-api.js';
 
 let reference =  null;
 async function run() {
@@ -10,17 +9,24 @@ async function run() {
     referenceData();
 }
 async function referenceData() {
-    const imgUrl = `Vaibhav Mittal.jpg`
+    const imgUrl = `Vaibhav Mittal.jpg`;
+    const mtcnnParams = {
+        minFaceSize: 200
+    };
+    const options = new faceapi.MtcnnOptions(mtcnnParams);
     console.log("Image aagiiii");
+    console.log(faceapi.fetchImage);
     const img = await faceapi.fetchImage(imgUrl);
-    const fullFaceDescription = await faceapi.detectSingleFace(img, new MtcnnOptions()).withFaceLandmarks().withFaceDescriptor();
-    
+    console.log("Helllo jiiiii - 1");
+    const fullFaceDescription = await faceapi.detectAllFaces(img, options).withFaceLandmarks().withFaceDescriptors();
+    console.log(fullFaceDescription,"Helllo jiiiii");
     if (!fullFaceDescription) {
         throw new Error(`no faces detected for ${"Vaibhav Mittal"}`);
     }
-    
-    const faceDescriptors = [fullFaceDescription.descriptor]
-    reference = [new faceapi.LabeledFaceDescriptors("Vaibhav Mittal", faceDescriptors)];
+    console.log("Helllo jiiiii");
+    const faceDescriptors = fullFaceDescription[0].descriptor;
+    console.log(faceDescriptors);
+    reference = [new faceapi.LabeledFaceDescriptors("Vaibhav Mittal", [faceDescriptors])];
     console.log(reference, "Hiii");
 }
 async function playFunc() {
@@ -30,27 +36,33 @@ async function playFunc() {
     const canvas = document.getElementById('overlay');
     const context = canvas.getContext('2d');
     context.clearRect(0, 0, canvas.width, canvas.height);
-    const options = new faceapi.MtcnnOptions(mtcnnParams)
+    const options = new faceapi.MtcnnOptions(mtcnnParams);
     const input = document.getElementById('inputVideo');
     const displaySize = { width: 640, height: 480 };
     faceapi.matchDimensions(canvas, displaySize);
     let fullFaceDescriptions = await faceapi.detectAllFaces(input, options).withFaceLandmarks().withFaceDescriptors();
     const resizedDetections = faceapi.resizeResults(fullFaceDescriptions, displaySize);
-    // if(reference) {
-    //     // const maxDescriptorDistance = 0.6
-    //     // const faceMatcher = new faceapi.FaceMatcher(reference, maxDescriptorDistance)
-    //     // const results = fullFaceDescriptions.map(fd => faceMatcher.findBestMatch(fd.descriptor));
-    //     // results.forEach((bestMatch, i) => {
-    //     //     const box = fullFaceDescriptions[i].detection.box
-    //     //     const text = bestMatch.toString()
-    //     //     const drawBox = new faceapi.draw.DrawBox(box, { label: text })
-    //     //     drawBox.draw(canvas)
-    //     // })
-    // }
-    // else {
+    let labeledDescriptors = reference;
+    if(labeledDescriptors) {
+        console.log(labeledDescriptors[0]);
+        const maxDescriptorDistance = 0.6
+        const faceMatcher = new faceapi.FaceMatcher(labeledDescriptors[0]);
+        const results = fullFaceDescriptions.map(fd => faceMatcher.findBestMatch(fd.descriptor));
+        results.forEach((bestMatch, i) => {
+            const box = fullFaceDescriptions[i].detection.box
+            const text = bestMatch.toString()
+            const drawBox = new faceapi.draw.DrawBox(box, { label: text })
+            drawBox.draw(canvas)
+        })
+        // console.log(JSON.parse(localStorage.getItem('fullFaceDescription')));
+        // const faceMatcher = new faceapi.FaceMatcher(JSON.parse(localStorage.getItem('fullFaceDescription')));
+        // const bestMatch = faceMatcher.findBestMatch(fullFaceDescriptions);
+        // console.log(bestMatch.toString())
+    }
+    else {
         faceapi.draw.drawDetections(canvas, resizedDetections)
         console.log(fullFaceDescriptions,1);
-    //}
+    }
     setTimeout(playFunc,500);
 }
 const Videocam = () => {
@@ -68,7 +80,7 @@ const Videocam = () => {
     },[]);
     return (
         <div className="margin">
-            <video style={{position: "absolute"}} id = "inputVideo" ref = {videoRef} autoPlay ={true}  muted></video>
+            <video style={{position: "absolute"}} id = "inputVideo" ref = {videoRef} onPlay = {() =>playFunc(videoRef)} autoPlay ={true}  muted></video>
             <canvas style={{position: "absolute"}} id="overlay" />
         </div>
     );
