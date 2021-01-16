3f7e3f5adf055e8bbf9d06f20916abdaee57aedb
mittalvaibhav1 <mittalvaibhavandroid@gmail.com>
2021-01-16 22:29:56 +0530

    footer

---
+++ smart-attendee-tool/src/components/Footer.js
@@ -0,0 +1,11 @@
+const Footer = () => {
+    return ( 
+        <footer>
+            <span>
+                Check out my work on <a href="https://github.com/Kritika1300" target="_blank">Github </a>
+            </span>
+        </footer>
+    );
+}
+
+export default Footer;
