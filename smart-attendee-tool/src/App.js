c0ff56ab33b81101dac14a39a8c4267c7a78c809
mittalvaibhav1 <mittalvaibhavandroid@gmail.com>
2021-01-16 22:29:47 +0530

    services

--- smart-attendee-tool/src/App.js
+++ smart-attendee-tool/src/App.js
@@ -1,11 +1,15 @@
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
+import Services from "./components/Services";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
       <Hero/>
+      <Services />
+      <Footer />
     </div>
   );
 }
