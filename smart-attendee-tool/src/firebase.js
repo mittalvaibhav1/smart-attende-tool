55ba8a7cc7f82b53cc933cee63807ff219c6935b
mittalvaibhav1 <mittalvaibhavandroid@gmail.com>
2021-01-17 23:51:43 +0530

    firebase config

---
+++ smart-attendee-tool/src/firebase.js
@@ -0,0 +1,12 @@
+
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDNrINEBQyzyt2dis7RdvA4fuLB_v8paiE",
+    authDomain: "smart-attendee-tool.firebaseapp.com",
+    projectId: "smart-attendee-tool",
+    storageBucket: "smart-attendee-tool.appspot.com",
+    messagingSenderId: "292503069701",
+    appId: "1:292503069701:web:e51d47affeaa5c308de86c",
+    measurementId: "G-GFEHW1GP7Q"
+};
