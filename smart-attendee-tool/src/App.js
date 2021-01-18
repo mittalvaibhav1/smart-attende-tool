28b25564f7643564851999ece10017ddb310f6b8
mittalvaibhav1 <mittalvaibhavandroid@gmail.com>
2021-01-18 17:15:35 +0530

    app

--- smart-attendee-tool/src/App.js
+++ smart-attendee-tool/src/App.js
@@ -2,6 +2,7 @@ import Login from "./components/Login";
 import {BrowserRouter, Redirect, Route, Switch} from 'react-router-dom';
 import UserContextProvider from "./contexts/UserContext";
 import Home from "./components/Home";
+import NewUser from "./components/NewUser";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <BrowserRouter>
         <UserContextProvider>
           <Switch>
+            <Route path="/login/new" component = {NewUser} />
             <Route path="/login" component = {Login} />
             <Route path="/" component = {Home} />
             <Redirect to="/" />
