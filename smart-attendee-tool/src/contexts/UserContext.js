2f8be1378b1a533efde2716ace8531bc39204589
mittalvaibhav1 <mittalvaibhavandroid@gmail.com>
2021-01-18 17:15:25 +0530

    User Context

--- smart-attendee-tool/src/contexts/UserContext.js
+++ smart-attendee-tool/src/contexts/UserContext.js
@@ -1,9 +1,12 @@
 import { createContext, useState } from 'react';
 
 export const UserContext = createContext();
+let user = sessionStorage.getItem('user');
+let session_user = user ? JSON.parse(user) : null;
 
 const UserContextProvider = (props) => {
-    const[user, setUser] = useState(null);
+    const[user, setUser] = useState(session_user);
+    
     return (  
         <UserContext.Provider value = {{user, setUser}}>
             {props.children}
