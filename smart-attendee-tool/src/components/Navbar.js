9fcc05fbc39c01d8a66a6ee343343ef42154509f
mittalvaibhav1 <mittalvaibhavandroid@gmail.com>
2021-01-18 17:15:54 +0530

    Navbar component

--- smart-attendee-tool/src/components/Navbar.js
+++ smart-attendee-tool/src/components/Navbar.js
@@ -1,16 +1,38 @@
+import ReactNotification from 'react-notifications-component';
+import { store } from 'react-notifications-component';
+
 const Navbar = () => {
+
+    const handleClick = (e) => {
+        e.preventDefault();
+            store.addNotification({
+                title: "Under Maintenance 🔨🔨",
+                message: "This feature is still under development, Please comeback later.",
+                type: "warning",
+                insert: "top",
+                container: "top-right", 
+                dismiss: {
+                    duration: 2000,
+                    onScreen: true
+                }
+            });
+    }
+
     return ( 
-        <div className="navbar">
-            <div className="navbar_logo">
-                <img src="./cgc-landran-logo.jpg" alt=""/>
+        <>
+            <div className="navbar">
+                <div className="navbar_logo">
+                    <img src="./cgc-landran-logo.jpg" alt=""/>
+                </div>
+                <span className="navbar_text">CGC Landran</span>
+                <div className="nav_items">
+                    <a onClick = {handleClick} href="/about">About</a>
+                    <a onClick = {handleClick} href="/contactus">Contact us</a>
+                    <a href="/login">Login</a>
+                </div>
             </div>
-            <span className="navbar_text">CGC Landran</span>
-            <div className="nav_items">
-                <a href="/about">About</a>
-                <a href="/contactus">Contact us</a>
-                <a href="/login">Login</a>
-            </div>
-        </div>
+            <ReactNotification/>
+        </>
     );
 }
 
